9e0da9d5cdc81ee92e5fec5ae25d7747f733d36f
Mohammed Faour <mohammed.faour@finn.com>
2023-10-23 13:39:17 +0200

    fix image imports

--- hubspot/get-contact.sh
+++ hubspot/get-contact.sh
@@ -7,8 +7,8 @@
 #
 # Optional parameters:
 # @raycast.packageName Hubspot
-# @raycast.icon images/hubspot.png
-# @raycast.iconDark images/hubspot.png.png
+# @raycast.icon ../images/hubspot.png
+# @raycast.iconDark ../images/hubspot.png
 # @raycast.argument1 { "type": "text", "placeholder": "ContactId", "percentEncoded": true }
 
 open https://app.hubspot.com/contacts/6028262/record/0-1/$1
--- hubspot/get-deal.sh
+++ hubspot/get-deal.sh
@@ -7,8 +7,8 @@
 #
 # Optional parameters:
 # @raycast.packageName Hubspot
-# @raycast.icon images/hubspot.png
-# @raycast.iconDark images/hubspot.png.png
+# @raycast.icon ../images/hubspot.png
+# @raycast.iconDark ../images/hubspot.png
 # @raycast.argument1 { "type": "text", "placeholder": "DealId", "percentEncoded": true }
 
 open https://app.hubspot.com/contacts/6028262/record/0-3/$1
